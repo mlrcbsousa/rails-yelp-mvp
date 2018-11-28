3ea788cf02eb2d4665267b3858a804a102c676d3
Manuel Sousa <mlrcbsousa@gmail.com>
2018-11-28 11:37:19 +0000

    rating must be integer

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,5 @@
 class Restaurant < ApplicationRecord
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian] }
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
   has_many :reviews, dependent: :destroy
 end
