d2fcb32e9bde5cea2729ab566b820dc7e4492d6a
Manuel Sousa <mlrcbsousa@gmail.com>
2018-11-28 11:42:33 +0000

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurants/new'
-  get 'restaurants/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[new create]
+  end
 end
